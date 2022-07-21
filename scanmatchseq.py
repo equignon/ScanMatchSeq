6dc9d1f62625c0b738efd5c82da002c2cfcaa9cb
Erwan Quignon <erwan.quignon@protonmail.com>
2022-07-21 15:12:13 +0200

    formating and removing default value searched

--- scanmatchseq.py
+++ scanmatchseq.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import sys
 import os
 import numpy as np
@@ -8,7 +6,10 @@ import csv
 import pathlib
 import argparse
 
-from Bio import SeqIO # For arm Mac user: might need to install Biopython from source as pip seems to install x64 version
+from Bio import (
+    SeqIO,
+)  # For arm Mac user: might need to install Biopython from source as pip seems to install x64 version
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -17,10 +18,19 @@ def main(argv):
     parser.add_argument("fasta_B", help="FASTA file of second sequence")
     parser.add_argument("values_A", help="Values corresponding to first sequence")
     parser.add_argument("values_B", help="Values corresponding to second sequence")
+    parser.add_argument(
+        "value_searched", help="Value searched in the two datasets", type=float
+    )
     parser.add_argument("--window", help="Window size", type=int, default=4)
-    parser.add_argument("--value_searched", help="Value searched in the two datasets", type=float, default=1.0)
-    parser.add_argument("--threshold", help="Threshold for the sliding window", type=int, default=2)
-    parser.add_argument("--test_type", help="Type of test to realize ('equal', 'superior', 'inferior', 'superior or equal', 'inferior or equal')", type=str, default="equal")
+    parser.add_argument(
+        "--threshold", help="Threshold for the sliding window", type=int, default=2
+    )
+    parser.add_argument(
+        "--test_type",
+        help="Type of test to realize ('equal', 'superior', 'inferior', 'superior or equal', 'inferior or equal')",
+        type=str,
+        default="equal",
+    )
 
     args = parser.parse_args()
 
@@ -34,7 +44,9 @@ def main(argv):
 
     # Sliding window on first sequence to look for sequences containing at least x times the searched value (default = 2)
 
-    df_results = pd.DataFrame(columns=['Position A', 'Sequence A', 'Sequence B', 'Position B'])
+    df_results = pd.DataFrame(
+        columns=["Position A", "Sequence A", "Sequence B", "Position B"]
+    )
 
     for i, row in df_seq_A.iterrows():
         sequence_A = ""
@@ -47,7 +59,7 @@ def main(argv):
         sequence_A = parse_A[0]
         nb_found = parse_A[1]
 
-        if(nb_found >= args.threshold):
+        if nb_found >= args.threshold:
             for k, row_b in df_seq_B.iterrows():
                 sequence_B = ""
                 j = k
@@ -65,19 +77,37 @@ def main(argv):
                     if seq_equal == True:
                         pos_A = str(i) + " - " + str(i + args.window - 1)
                         pos_B = str(k) + " - " + str(k + args.window - 1)
-                        new_row = pd.DataFrame([{'Position A': pos_A, 'Sequence A': sequence_A, 'Sequence B': sequence_B, 'Position B': pos_B}])
-                        df_results = pd.concat([df_results, new_row], axis = 0)
-                    
+                        new_row = pd.DataFrame(
+                            [
+                                {
+                                    "Position A": pos_A,
+                                    "Sequence A": sequence_A,
+                                    "Sequence B": sequence_B,
+                                    "Position B": pos_B,
+                                }
+                            ]
+                        )
+                        df_results = pd.concat([df_results, new_row], axis=0)
+
                     sequence_B = sequence_B[::-1]
                     seq_equal = compare_sequence(args, sequence_A, sequence_B)
 
                     if seq_equal == True:
                         pos_A = str(i) + " - " + str(i + args.window - 1)
                         pos_B = str(k) + " - " + str(k + args.window - 1)
-                        new_row = pd.DataFrame([{'Position A': pos_A, 'Sequence A': sequence_A, 'Sequence B': sequence_B, 'Position B': pos_B}])
-                        df_results = pd.concat([df_results, new_row], axis = 0)
+                        new_row = pd.DataFrame(
+                            [
+                                {
+                                    "Position A": pos_A,
+                                    "Sequence A": sequence_A,
+                                    "Sequence B": sequence_B,
+                                    "Position B": pos_B,
+                                }
+                            ]
+                        )
+                        df_results = pd.concat([df_results, new_row], axis=0)
 
-    df_results.to_csv(r'output.csv', sep='\t', index = False, header = True)
+    df_results.to_csv(r"output.csv", sep="\t", index=False, header=True)
 
     df_chord_A = pd.DataFrame()
     df_chord_B = pd.DataFrame()
@@ -85,132 +115,142 @@ def main(argv):
         pos_A = row[0].split(" - ")
         start_A = int(pos_A[0])
         end_A = int(pos_A[1])
-        new_row = pd.DataFrame([{'Sequence':"SeqA", 'Sequence_A': start_A, 'Sequence_B': end_A}])
-        df_chord_A = pd.concat([df_chord_A, new_row], axis = 0)
+        new_row = pd.DataFrame(
+            [{"Sequence": "SeqA", "Sequence_A": start_A, "Sequence_B": end_A}]
+        )
+        df_chord_A = pd.concat([df_chord_A, new_row], axis=0)
 
         pos_B = row[3].split(" - ")
         start_B = int(pos_B[0])
         end_B = int(pos_B[1])
-        new_row = pd.DataFrame([{'Sequence':"SeqB", 'Sequence_A': start_B, 'Sequence_B': end_B}])
-        df_chord_B = pd.concat([df_chord_B, new_row], axis = 0)
+        new_row = pd.DataFrame(
+            [{"Sequence": "SeqB", "Sequence_A": start_B, "Sequence_B": end_B}]
+        )
+        df_chord_B = pd.concat([df_chord_B, new_row], axis=0)
 
-    df_chord_A.to_csv(r'chord_diag_A.csv', sep='\t', index = False, header = True)
-    df_chord_B.to_csv(r'chord_diag_B.csv', sep='\t', index = False, header = True)
+    df_chord_A.to_csv(r"chord_diag_A.csv", sep="\t", index=False, header=True)
+    df_chord_B.to_csv(r"chord_diag_B.csv", sep="\t", index=False, header=True)
+
 
 def add_dataset(df, file, value):
     for seq_record in SeqIO.parse(file, "fasta"):
-        df = pd.DataFrame(index = np.arange(1, len(seq_record.seq)+1), columns = ['Nucleotide', 'Reactivity'])
+        df = pd.DataFrame(
+            index=np.arange(1, len(seq_record.seq) + 1),
+            columns=["Nucleotide", "Reactivity"],
+        )
         x = 1
         for p in seq_record.seq:
-            df.at[x, 'Nucleotide'] = p
+            df.at[x, "Nucleotide"] = p
             x += 1
         with open(value) as f:
-            for line in csv.reader(f, dialect = "excel-tab"):
-                if(line[1] == "" or line[1] == "#N/A"):
-                    df.at[int(line[0]), 'Reactivity'] = float('NaN')
+            for line in csv.reader(f, dialect="excel-tab"):
+                if line[1] == "" or line[1] == "#N/A":
+                    df.at[int(line[0]), "Reactivity"] = float("NaN")
                 else:
-                    df.at[int(line[0]), 'Reactivity'] = float(line[1])
+                    df.at[int(line[0]), "Reactivity"] = float(line[1])
     return df
+
 
 def check_value(args, df, seq, i, j):
     nb_found = 0
     if args.test_type == "equal":
         while j < (i + args.window):
             # Generating sequence contained in the windows for sequence A
-            seq += df.at[j, 'Nucleotide']
+            seq += df.at[j, "Nucleotide"]
 
             # Counting the number of nucleotides that reaches the threshold given
-            if(pd.isna(df.at[j, 'Reactivity'])):
+            if pd.isna(df.at[j, "Reactivity"]):
                 pass
             else:
-                if(float(df.at[j, 'Reactivity']) == args.value_searched):
+                if float(df.at[j, "Reactivity"]) == args.value_searched:
                     nb_found += 1
             j += 1
 
     if args.test_type == "superior":
         while j < (i + args.window):
             # Generating sequence contained in the windows for sequence A
-            seq += df.at[j, 'Nucleotide']
+            seq += df.at[j, "Nucleotide"]
 
             # Counting the number of nucleotides that reaches the threshold given
-            if(pd.isna(df.at[j, 'Reactivity'])):
+            if pd.isna(df.at[j, "Reactivity"]):
                 pass
             else:
-                if(float(df.at[j, 'Reactivity']) > args.value_searched):
+                if float(df.at[j, "Reactivity"]) > args.value_searched:
                     nb_found += 1
             j += 1
 
     if args.test_type == "inferior":
         while j < (i + args.window):
             # Generating sequence contained in the windows for sequence A
-            seq += df.at[j, 'Nucleotide']
+            seq += df.at[j, "Nucleotide"]
 
             # Counting the number of nucleotides that reaches the threshold given
-            if(pd.isna(df.at[j, 'Reactivity'])):
+            if pd.isna(df.at[j, "Reactivity"]):
                 pass
             else:
-                if(float(df.at[j, 'Reactivity']) < args.value_searched):
+                if float(df.at[j, "Reactivity"]) < args.value_searched:
                     nb_found += 1
             j += 1
 
     if args.test_type == "superior or equal":
         while j < (i + args.window):
             # Generating sequence contained in the windows for sequence A
-            seq += df.at[j, 'Nucleotide']
+            seq += df.at[j, "Nucleotide"]
 
             # Counting the number of nucleotides that reaches the threshold given
-            if(pd.isna(df.at[j, 'Reactivity'])):
+            if pd.isna(df.at[j, "Reactivity"]):
                 pass
             else:
-                if(float(df.at[j, 'Reactivity']) >= args.value_searched):
+                if float(df.at[j, "Reactivity"]) >= args.value_searched:
                     nb_found += 1
             j += 1
-
 
     if args.test_type == "inferior or equal":
         while j < (i + args.window):
             # Generating sequence contained in the windows for sequence A
-            seq += df.at[j, 'Nucleotide']
+            seq += df.at[j, "Nucleotide"]
 
             # Counting the number of nucleotides that reaches the threshold given
-            if(pd.isna(df.at[j, 'Reactivity'])):
+            if pd.isna(df.at[j, "Reactivity"]):
                 pass
             else:
-                if(float(df.at[j, 'Reactivity']) <= args.value_searched):
+                if float(df.at[j, "Reactivity"]) <= args.value_searched:
                     nb_found += 1
             j += 1
 
     return seq, nb_found
 
+
 def compare_sequence(args, seq_A, seq_B):
     c = 1
     length = 0
     while c <= args.window:
-        if(seq_A[c-1] == "A"):
-            if(seq_B)[c-1] == "U":
+        if seq_A[c - 1] == "A":
+            if (seq_B)[c - 1] == "U":
                 length += 1
         else:
             pass
-        if(seq_A[c-1] == "U"):
-            if seq_B[c-1] == "A" or seq_B[c-1] == "G":
+        if seq_A[c - 1] == "U":
+            if seq_B[c - 1] == "A" or seq_B[c - 1] == "G":
                 length += 1
         else:
             pass
-        if(seq_A[c-1] == "G"):
-            if seq_B[c-1] == "C" or seq_B[c-1] == "U":
+        if seq_A[c - 1] == "G":
+            if seq_B[c - 1] == "C" or seq_B[c - 1] == "U":
                 length += 1
             else:
                 pass
-        if(seq_A[c-1] == "C"):
-            if seq_B[c-1] == "G":
+        if seq_A[c - 1] == "C":
+            if seq_B[c - 1] == "G":
                 length += 1
             else:
                 pass
         c += 1
-    if(length == args.window):
+    if length == args.window:
         return True
     else:
         return False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(sys.argv)
